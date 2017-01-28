17ee143a63721964aa6ce3a06fb7e982529337f6
Fernando Gandarilla <fgandarilla@gcib.net>
2017-01-28 04:16:05 -0800

    Validate session
    >> Validate if account was been session

--- application/controllers/provider.php
+++ application/controllers/provider.php
@@ -23,50 +23,59 @@ class provider extends CI_Controller
 
 	function provider_insert()
 	{
-		$this->form_validation->set_rules('fname', 'First Name', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('mname', 'Middle Name', 'trim|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('lname', 'Last Name', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('phone', 'Phone', 'trim|min_length[3]|max_length[10]|xss_clean');
-		$this->form_validation->set_rules('phone_2', 'Phone 2', 'trim|min_length[3]|max_length[10]|xss_clean');
-		$this->form_validation->set_rules('street', 'Street', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('city', 'City', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('state', 'State', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('zip_code', 'Zip Code', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('specialty', 'Specialty', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('licence_num', 'Licence Number', 'trim|required|min_length[3]|max_length[30]|xss_clean');
-		$this->form_validation->set_rules('tax_id', 'Tax ID', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+		if ($this->session->userdata('id_user'))
+		{
+			$this->form_validation->set_rules('fname', 'First Name', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('mname', 'Middle Name', 'trim|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('lname', 'Last Name', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('phone', 'Phone', 'trim|min_length[3]|max_length[10]|xss_clean');
+			$this->form_validation->set_rules('phone_2', 'Phone 2', 'trim|min_length[3]|max_length[10]|xss_clean');
+			$this->form_validation->set_rules('street', 'Street', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('city', 'City', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('state', 'State', 'trim|required|xss_clean');
+			$this->form_validation->set_rules('zip_code', 'Zip Code', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('specialty', 'Specialty', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('licence_num', 'Licence Number', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('tax_id', 'Tax ID', 'trim|required|min_length[3]|max_length[30]|xss_clean');
 
-		if ($this->form_validation->run() == FALSE)
-		{
-			$this->load->view('accounts/provider/provider_create');
-		}
-		else
-		{
-			$data = array(
-				'fname'	=> $this->input->post('fname'),
-				'mname'	=> $this->input->post('mname'),
-				'lname'	=> $this->input->post('lname'),
-				'phone' => $this->input->post('phone'),
-				'phone_2'   => $this->input->post('phone_2'),
-				'street'=> $this->input->post('street'),
-				'city'	=> $this->input->post('city'),
-				'state'	=> $this->input->post('state'),
-				'zip_code'	=> $this->input->post('zip_code'),
-				'specialty'	=> $this->input->post('specialty'),
-				'licence_num'	=> $this->input->post('licence_num'),
-				'tax_id'	=> $this->input->post('tax_id'),
-			);
-			if ($this->provider_model->insert_provider($data))
+			if ($this->form_validation->run() == FALSE)
 			{
-				$this->session->set_flashdata('msg','<script>Materialize.toast("You are Successfully Registered! Please login to access your Profile!", 5000);</script>');
-				redirect('provider/provider_create');
+				$this->load->view('accounts/provider/provider_create');
 			}
 			else
 			{
-				// error
-				$this->session->set_flashdata('msg','<script>Materialize.toast("Oops! Error.  Please try again later!!!", 5000);</script>');
-				redirect('provider/provider_create');
+				$data = array(
+					'fname'	=> $this->input->post('fname'),
+					'mname'	=> $this->input->post('mname'),
+					'lname'	=> $this->input->post('lname'),
+					'phone' => $this->input->post('phone'),
+					'phone_2'   => $this->input->post('phone_2'),
+					'street'=> $this->input->post('street'),
+					'city'	=> $this->input->post('city'),
+					'state'	=> $this->input->post('state'),
+					'zip_code'	=> $this->input->post('zip_code'),
+					'specialty'	=> $this->input->post('specialty'),
+					'licence_num'	=> $this->input->post('licence_num'),
+					'tax_id'	=> $this->input->post('tax_id'),
+				);
+				if ($this->provider_model->insert_provider($data))
+				{
+					$this->session->set_flashdata('msg','<script>Materialize.toast("You are Successfully Registered! Please login to access your Profile!", 5000);</script>');
+					redirect('provider/provider_create');
+				}
+				else
+				{
+					// error
+					$this->session->set_flashdata('msg','<script>Materialize.toast("Oops! Error.  Please try again later!!!", 5000);</script>');
+					redirect('provider/provider_create');
+				}
 			}
+		}
+		else
+		{
+			$data['user'] = $this->user_model->get_user_by_id($this->session->userdata('id_user'));
+			redirect('home');
+
 		}
 	}
 }
